2e265a5cea54db2dd9ec015bd4b9667da1380aa8
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-06-15 04:18:51 +0300

    Added Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
 
+mongoose.connect(keys.mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
 const app = express();
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
