251a8d763cdcf0a2ee38ce260556cb00c463d790
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-06-23 04:21:38 +0300

    Refactored passport promise to ES2017 syntax

--- app.js
+++ app.js
@@ -25,4 +25,7 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.log(`> Now serving Express on port ${PORT}`);
+});
