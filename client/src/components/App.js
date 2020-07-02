6867c410bcd1e025611809b3511a2abf51e144ea
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-07-02 04:06:00 +0300

    Added Redux and started with Reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-const App = () => (
-  <div>
-    <h1>Hi There!</h1>
-  </div>
-);
+import * as actions from "../actions";
+import Header from "./Header";
 
-export default App;
+const Bla = () => <h1>HI!!!</h1>;
+const Users = () => <h1>Users</h1>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Bla} />
+          <Route exact path="/users" component={Users} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+// const App = () => (
+//   <div className="container">
+//     <Header />
+//     <Switch>
+//       <Route exact path="/" component={Bla} />
+//       <Route exact path="/users" component={Users} />
+//     </Switch>
+//   </div>
+// );
+
+export default connect(null, actions)(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => (
+  <nav>
+    <div className="nav-wrapper">
+      <a className="left brand-logo">Emaily</a>
+      <ul className="right">
+        <li>
+          <a>Login With Google</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Header;
