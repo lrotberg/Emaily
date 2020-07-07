a1db02bd10aedd442368bfa90bed2944793f2613
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-07-07 13:15:00 +0300

    Set up Header routes and display

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const Header = () => (
-  <nav>
-    <div className="nav-wrapper">
-      <a className="left brand-logo">Emaily</a>
-      <ul className="right">
-        <li>
-          <a>Login With Google</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
 
-export default Header;
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo" href="/">
+            Emaily
+          </a>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+// const Header = () => {
+//   return (
+//     <nav>
+//       <div className="nav-wrapper">
+//         <a className="left brand-logo">Emaily</a>
+//         <ul className="right">
+//           <li>
+//             <a>Login With Google</a>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+const mapStateToProps = ({ auth }) => ({ auth });
+
+export default connect(mapStateToProps)(Header);
